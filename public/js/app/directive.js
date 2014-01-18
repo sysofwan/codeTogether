42f03e8163fa0b2ceb009842e92b020bd11a8f09
Sofwan Haddad <sysofwan@Sofwans-MacBook-Pro.local>
2014-01-18 06:17:54 -0500

    added styling

--- public/js/app/directive.js
+++ public/js/app/directive.js
@@ -1,6 +1,7 @@
 app.directive('liveEdit', function() {
 	var link = function(scope, element, attrs) {
 		var editor = ace.edit(element.attr('id'));
+		editor.setTheme("ace/theme/monokai");
 		console.log(scope.title);
 		sharejs.open(scope.title, 'text', function(error, doc) {
 			doc.attach_ace(editor);
