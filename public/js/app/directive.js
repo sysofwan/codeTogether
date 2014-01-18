b4f4c0586ce3988558894c064b43190b66535ee1
moham601 <moham601@umn.edu>
2014-01-18 07:32:36 -0600

    Auto complete

--- public/js/app/directive.js
+++ public/js/app/directive.js
@@ -1,10 +1,15 @@
 app.directive('liveEdit', function() {
 	var link = function(scope, element, attrs) {
+		ace.require("ace/ext/language_tools");
 		var editor = ace.edit(element.attr('id'));
 		editor.setTheme("ace/theme/monokai");
 		editor.setShowPrintMargin(false);
 		editor.focus();
 		console.log(scope.title);
+		editor.setOptions({
+    	enableBasicAutocompletion: true,
+    	 enableSnippets: true
+		});
 
 		sharejs.open(scope.title, 'text', function(error, doc) {
 			doc.attach_ace(editor);
