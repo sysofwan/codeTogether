d88f02b7bcceec66a39db6d00f3e39e310ff2baa
moham601 <moham601@umn.edu>
2014-01-18 05:29:36 -0600

    setfocus

--- public/js/app/directive.js
+++ public/js/app/directive.js
@@ -2,7 +2,9 @@ app.directive('liveEdit', function() {
 	var link = function(scope, element, attrs) {
 		var editor = ace.edit(element.attr('id'));
 		editor.setTheme("ace/theme/monokai");
+		editor.focus();
 		console.log(scope.title);
+
 		sharejs.open(scope.title, 'text', function(error, doc) {
 			doc.attach_ace(editor);
 		});
